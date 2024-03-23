75ed1ef4da7bdbffa8a219f55c4dbd670d645180
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2024-03-24 01:25:10 +0400

    add simple logic for deployment testing

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Starting")
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg: "Working!"
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.status(200).json({
+        msg: "Login user!"
+    });
+});
+
+app.get('/register', (req, res) => {
+    res.status(200).json({
+        msg: "Register user!"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`backend running on ${PORT}`);
+});
