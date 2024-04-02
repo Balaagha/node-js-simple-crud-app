57044ccc1bb4950f99bd032f6b0601ec9e2a53f9
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2024-04-02 17:42:08 +0400

    add db connection

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER_NAME = process.env.DB_USER_NAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST_NAME = process.env.DB_HOST_NAME;
+
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: DB_HOST_NAME,
+    user: DB_USER_NAME,
+    password: DB_PASSWORD,
+    database: DB_NAME
+});
 
+// Veritabanına bağlan
+connection.connect(error => {
+    if (error) throw error;
+    console.log("Veritabanına başarıyla bağlandı.");
+});
+
+// Kullanıcıları listele
+app.get('/users', (req, res) => {
+    connection.query('SELECT * FROM users', (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
+});
 
 app.get('/', (req, res) => {
     res.status(200).json({
